61de3e92254f38c7ec160340961eb6005463f495
Cauê Licce <caue@dallasmotel.com.br>
2026-04-22 22:11:43 -0300

    Create script.js

---
+++ portfolio/prova3_ex1/script.js
@@ -0,0 +1,10 @@
+function mostrar() {
+
+    var div = document.getElementById("imagem");
+
+    var img = document.createElement("img");
+    img.src = "img/Tabela_Jogos.png";
+    img.width = 500;
+
+    div.appendChild(img);
+}
