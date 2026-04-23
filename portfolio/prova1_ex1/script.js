4db5882171307bd8aec1de71577c0d275e1bb0b2
Cauê Licce <caue@dallasmotel.com.br>
2026-04-22 21:49:26 -0300

    Update script.js

--- portfolio/prova1_ex1/script.js
+++ portfolio/prova1_ex1/script.js
@@ -2,7 +2,7 @@ function mostrarImagem() {
     var div = document.getElementById("resultado");
 
     var img = document.createElement("img");
-    img.src = "img/tabela.png"; // caminho da imagem (ajuste se necessário)
+    img.src = "img/Tabela_Jogos.png";
     img.width = 500;
 
     div.appendChild(img);
